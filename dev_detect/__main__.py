0cd8bdb3a627463c015792c06706852307219a07
Martin Matějek <martin.matejek@nic.cz>
2019-04-07 23:07:24 +0200

    Refactor main() into smaller functions

--- dev_detect/__main__.py
+++ dev_detect/__main__.py
@@ -12,6 +12,7 @@ IPV4 = 2
 IPV6 = 10
 
 # default watched interfaces
+# TODO: get list from uci?
 watched_interfaces = ['br-lan']
 
 
@@ -30,54 +31,60 @@ def new_device_notify(mac, iface):
     thread.start()
 
 
+def get_interfaces(ipr):
+    active_interfaces = {}
+    links = ipr.get_links()
+
+    for l in links:
+        ifname = l.get_attr('IFLA_IFNAME')
+
+        # TODO: check if interface is also up
+        if ifname in watched_interfaces:
+            active_interfaces[l['index']] = ifname
+
+    return active_interfaces
+
+
+def detect_devices(ipr, interfaces, known_devices):
+    while True:
+        messages = ipr.get()
+
+        for message in messages:
+            # we are interested only in new devices on selected interfaces
+            if message['ifindex'] in interfaces and message['event'] == 'RTM_NEWNEIGH':
+                mac = message.get_attr('NDA_LLADDR')
+                ip = message.get_attr('NDA_DST')
+
+                # sometimes IP or MAC address is missing in netlink message
+                if not mac or not ip:
+                    continue
+
+                if mac not in known_devices:
+                    known_devices[mac] = [ip]
+                    # TODO: write throught into persistent database
+
+                    new_device_notify(mac, 'lan')
+
+                    if message['family'] == IPV4:
+                        ipver = 'v4'
+                    elif message['family'] == IPV6:
+                        ipver = 'v6'
+
+                    print("New device detected MAC: {} | IP{} address: {}".format(mac, ipver, ip))
+
+                if ip not in known_devices[mac]:
+                    known_devices[mac].append(ip)
+
+
 def main():
     known_devices = {}
-
     # TODO: load already known devices from database
 
-    interfaces = {}
-
     with IPRoute() as ipr:
-        links = ipr.get_links()
+        ipr.bind()  # subscribe to netlink broadcast
 
-        for l in links:
-            ifname = l.get_attr('IFLA_IFNAME')
-
-            # TODO: check if interface is also up
-            if ifname in watched_interfaces:
-                interfaces[l['index']] = ifname
-
-        # subscribe to broadcast
-        ipr.bind()
-
-        while True:
-            messages = ipr.get()
-
-            for message in messages:
-                # we are interested only in new devices on selected interfaces
-                if message['ifindex'] in interfaces and message['event'] == 'RTM_NEWNEIGH':
-                    mac = message.get_attr('NDA_LLADDR')
-                    ip = message.get_attr('NDA_DST')
-
-                    # sometimes IP or MAC address is missing in netlink message
-                    if not mac or not ip:
-                        continue
-
-                    if mac not in known_devices:
-                        known_devices[mac] = [ip]
-                        # TODO: write throught into persistent database
-
-                        new_device_notify(mac, 'lan')
-
-                        if message['family'] == IPV4:
-                            ipver = 'v4'
-                        elif message['family'] == IPV6:
-                            ipver = 'v6'
-
-                        print("New device detected MAC: {} | IP{} address: {}".format(mac, ipver, ip))
-
-                    if ip not in known_devices[mac]:
-                        known_devices[mac].append(ip)
+        interfaces = get_interfaces()
+        detect_devices(ipr, interfaces, known_devices)
 
 
 if __name__ == "__main__":
