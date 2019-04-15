23a554209db8e11d21a2dd826626aca9f5b49225
Martin Matějek <martin.matejek@nic.cz>
2019-04-16 01:24:30 +0200

    Configure databases location in uci

--- dev_detect/storage.py
+++ dev_detect/storage.py
@@ -1,6 +1,8 @@
 import logging
 import sqlite3
 
+from euci import EUci
+
 logger = logging.getLogger(__name__)
 
 # TODO: merge query execution into single function
@@ -9,11 +11,21 @@ logger = logging.getLogger(__name__)
 class Storage:
     """Database storage of known devices"""
 
-    def __init__(self, persistent):
+    def __init__(self, persistent=False):
+        self.uci = EUci()
+
+        persistent_db_path = self.uci.get('dev-detect.storage.persistent_db_path')
+        if not persistent_db_path:
+            persistent_db_path = '/srv/dev-detect.db'
+
+        volatile_db_path = self.uci.get('dev-detect.storage.volatile_db_path')
+        if not volatile_db_path:
+            volatile_db_path = '/tmp/dev-detect.db'
+
         if persistent:
-            self.db_path = '/srv/dev-detect.db'
+            self.db_path = persistent_db_path
         else:
-            self.db_path = '/tmp/dev-detect.db'
+            self.db_path = volatile_db_path
 
         self._init_connection()
 
