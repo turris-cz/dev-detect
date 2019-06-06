ced03d5f7e5895b47a3a61774249ef338cafb967
Martin Matějek <martin.matejek@nic.cz>
2019-06-06 18:04:02 +0200

    fix tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,18 +1,18 @@
 import pytest
 import os
 
-from dev_detect.storage import Storage
+from dev_detect.storage import DatabaseStorage
 
 
 @pytest.fixture(scope='function')
 def storage(request, tmpdir):
     database_dir = tmpdir.mkdir('dev-detect')
     db_path = os.path.join(database_dir, 'dev-detect.db')
-    mystorage = Storage(db_path)
+    storage = DatabaseStorage(db_path)
 
     if hasattr(request, 'param'):
         for item in request.param:
             print("Storing item {}".format(item))
-            mystorage.store(item)
+            storage.store(item)
 
-    yield mystorage
+    yield storage
