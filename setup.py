36480a7764867968d185876886b2edf39e48e6ed
Martin Matějek <martin.matejek@nic.cz>
2020-09-22 16:34:30 +0200

    setup.py: update gitlab URL

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ setup(
     author='CZ.NIC, z.s.p.o. (http://www.nic.cz/)',
     author_email='packaging@turris.cz',
     packages=['dev_detect'],
-    url='https://gitlab.labs.nic.cz/turris/dev-detect',
+    url='https://gitlab.nic.cz/turris/dev-detect',
     license='GPL-3.0-only',
     description='Small utility to detect devices on local network',
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     install_requires=[
         'pyroute2',
-        'pyuci @ git+https://gitlab.labs.nic.cz/turris/pyuci.git',
+        'pyuci @ git+https://gitlab.nic.cz/turris/pyuci.git',
     ],
     entry_points={
         'console_scripts': [
